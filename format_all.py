f70b5587a347fc370cb38a6724998396e1ea0974
Chuyang Chen <chuyangchen2018@outlook.com>
2025-07-03 21:42:26 -0400

    Update subproject references in `phdkit` and `watchcat`
    - Change commit reference in `phdkit` to `949a6ee`
    - Change commit reference in `watchcat` to `2b0aa32`
    - Improve formatting in `format_all.py`

--- format_all.py
+++ format_all.py
@@ -11,11 +11,13 @@ import sys
 
 WORKSPACE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with open(os.path.join(WORKSPACE_ROOT, "pyproject.toml"), "rb") as f:
         pyproject_config = tomllib.load(f)
 
-    subprojects = pyproject_config.get("tool", {}).get("uv", {}).get("workspace", [])["members"]
+    subprojects = (
+        pyproject_config.get("tool", {}).get("uv", {}).get("workspace", [])["members"]
+    )
 
     for subproject in subprojects:
         print(f"Formatting subproject: {subproject}")
@@ -23,16 +25,19 @@ if __name__ == '__main__':
             ["uv", "run", os.path.join(WORKSPACE_ROOT, subproject, "format.py")],
             check=True,
             stdout=sys.stdout,
-            stderr=sys.stderr
+            stderr=sys.stderr,
         )
 
     print("Formatting Python files in the workspace root...")
-    python_files = list(map(lambda f: os.path.join(WORKSPACE_ROOT, f), [
-        f for f in os.listdir(WORKSPACE_ROOT) if f.endswith(".py")
-    ]))
+    python_files = list(
+        map(
+            lambda f: os.path.join(WORKSPACE_ROOT, f),
+            [f for f in os.listdir(WORKSPACE_ROOT) if f.endswith(".py")],
+        )
+    )
     subprocess.run(
         ["uvx", "ruff", "format", *python_files],
         check=True,
         stdout=sys.stdout,
-        stderr=sys.stderr
+        stderr=sys.stderr,
     )
